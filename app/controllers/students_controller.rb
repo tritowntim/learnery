435e79f42342d2d55622748139085148b8f113ee
Tim Dussinger <tritowntim@gmail.com>
2012-11-23 17:20:48 -0800

    Basic new, create, edit, update for students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,9 +1,43 @@
 class StudentsController < ApplicationController
-	
+
 	def index
+		@students = Student.all
 	end
 
 	def show
+		@student = Student.find(params[:id])
+
+		respond_to do |format|
+			format.html 
+			format.json { render :json => @student } 
+			format.xml { render :xml => @student } 
+		end
+	end
+
+	def new
+		@student = Student.new
+	end
+
+	def create
+		@student = Student.new(params[:student])
+		if @student.save
+			redirect_to @student
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@student = Student.find(params[:id])
+	end
+
+	def update
+		@student = Student.find(params[:id])
+		if @student.update_attributes(params[:student])
+			redirect_to @student
+		else
+			render :edit
+		end		
 	end
 
 end
